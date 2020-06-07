8c64d8dea0ec90adcb87e9f24e01b74542dcafa2
varsha <vmusham@gmail.com>
2020-06-07 17:37:49 +0530

    Data Binding

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+
+
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  
+  dishes: Dish[] = DISHES;
+
+  selectedDish: Dish;
+
+  ngOnInit(): void {
+  }
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
+  }
+
+}
